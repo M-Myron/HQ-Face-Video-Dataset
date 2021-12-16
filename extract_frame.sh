9f08bdb9139ac0613bd020307c39ddafccf7fcb1
Myron <43957936+M-Myron@users.noreply.github.com>
2021-12-16 14:24:35 +0000

    Update extract_frame.sh
    Add extract .wav part

--- extract_frame.sh
+++ extract_frame.sh
@@ -2,3 +2,4 @@ mkdir apple
 mkdir apple_2fps
 ffmpeg -i apple.webm -f image2 -vf fps=2 -qscale:v 2 ./apple_2fps/img_%05d.jpg
 ffmpeg -i apple.webm -f image2 -vf fps=30 -qscale:v 2 ./apple/img_%05d.jpg
+ffmpeg -i apple.webm -f wav -ac 1 -ar 16000 ./audio/apple.wav
